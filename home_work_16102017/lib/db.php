4fdf334df813a6a0b9a5994042f7accb39ce2841
kreat1v <kostyakomarov7@gmail.com>
2017-12-03 17:05:35 +0200

    интернет магазин

--- home_work_16102017/lib/db.php
+++ home_work_16102017/lib/db.php
@@ -19,29 +19,18 @@ $tablesMap = [
     'admins' => 'admins'
 ];
 
-//function login($login, $password)
-//{
-//    global $connection;
-//
-//    $result = mysqli_query($connection, "SELECT * FROM {$GLOBALS['tablesMap']['admins']};");
-//    while ($authorization = mysqli_fetch_assoc($result)){
-//        if ($authorization['login'] === $login && $authorization['password'] === $password) {
-//            return true;
-//        }
-//    }
-//    return false;
-//}
-
+// авторизация
 function login($login, $password)
 {
-	global $connection;
+    global $connection;
 
-	$result = mysqli_query($connection, "SELECT * FROM {$GLOBALS['tablesMap']['admins']} WHERE login = $login");
-	$authorization = mysqli_fetch_assoc($result);
-	if ($authorization['password'] === $password) {
-		return true;
-	}
-	return false;
+    $result = mysqli_query($connection, "SELECT * FROM {$GLOBALS['tablesMap']['admins']};");
+    while ($authorization = mysqli_fetch_assoc($result)){
+        if ($authorization['login'] === $login && $authorization['password'] === $password) {
+            return true;
+        }
+    }
+    return false;
 }
 
 // Получение списка
